f54af80b24cac935585aee851b0875a465cc9979
kawai011231 <yousuke011231@gmail.com>
2022-09-14 03:24:10 +0900

    スタイルを修正しました

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import MemoPage from './memo/MemoPage'
 function App() {
   return (
     <div>
-      <h1 className="bg-primary text-white display-4 ">React</h1>
+      <h1 className="bg-info text-white display-4 heading">TODO管理</h1>
       <div className="container">
-        <h4 className="my-3">Memo.</h4>
         <MemoPage />
       </div>
     </div>
